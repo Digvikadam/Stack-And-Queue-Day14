f454d42d20f824abc71b4c92bb5f2bde12114cc9
[Digvikadam] <[digvikadam168@gmail.com]>
2021-11-07 20:25:45 +0530

    Added: Code for Create Queue

---
+++ src/com/bridgelabz/StackAndQueueMain.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class StackAndQueueMain {
+	 public static void main(String[] args) {
+	        System.out.println("Welcome to the stack and queue program");
+	        Operation operation = new Operation();
+	            Scanner scanner = new Scanner(System.in);
+	            System.out.println("Press '1' to add element at the start \n" + "Enter 2 to show the peek element\n" +
+	                    "Enter 3 to show the pop \n " + "Enter 4 to queue the element");
+	            switch (scanner.nextInt()) {
+	                case 1:
+	                    operation.addElement();
+	                    break;
+	                case 2:
+	                    operation.peek();
+	                    break;
+	                case 3:
+	                    operation.pop();
+	                    break;
+	                case 4:
+	                    operation.queueElement();
+	                    break;
+	            }
+	            scanner.close();
+	        }
+}
