0804bf27d06481c94263def7cc4343903166f368
[Digvikadam] <[digvikadam168@gmail.com]>
2021-11-07 20:31:58 +0530

    Added: Code for Dqueue the Queue

--- src/com/bridgelabz/StackAndQueueMain.java
+++ src/com/bridgelabz/StackAndQueueMain.java
@@ -3,26 +3,29 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class StackAndQueueMain {
-	 public static void main(String[] args) {
-	        System.out.println("Welcome to the stack and queue program");
-	        Operation operation = new Operation();
-	            Scanner scanner = new Scanner(System.in);
-	            System.out.println("Press '1' to add element at the start \n" + "Enter 2 to show the peek element\n" +
-	                    "Enter 3 to show the pop \n " + "Enter 4 to queue the element");
-	            switch (scanner.nextInt()) {
-	                case 1:
-	                    operation.addElement();
-	                    break;
-	                case 2:
-	                    operation.peek();
-	                    break;
-	                case 3:
-	                    operation.pop();
-	                    break;
-	                case 4:
-	                    operation.queueElement();
-	                    break;
-	            }
-	            scanner.close();
-	        }
+	public static void main(String[] args) {
+		System.out.println("Welcome to the stack and queue program");
+		Operation operation = new Operation();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Press '1' to add element at the start \n" + "Enter 2 to show the peek element\n" +
+				"Enter 3 to show the pop \n " + "Enter 4 to queue the element\n" + "Enter 5 to dequeue the element");
+		switch (scanner.nextInt()) {
+		case 1:
+			operation.addElement();
+			break;
+		case 2:
+			operation.peek();
+			break;
+		case 3:
+			operation.pop();
+			break;
+		case 4:
+			operation.queueElement();
+			break;
+		case 5:
+			operation.dequeueElement();
+			break;
+		}
+		scanner.close();
+	}
 }
