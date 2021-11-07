492c69f2245800ddf2cf3978864b8e8effbe16ad
[Digvikadam] <[digvikadam168@gmail.com]>
2021-11-07 20:18:26 +0530

    Added: Code for Pop and Peek in Stack

--- src/com/bridgelabz/Operation.java
+++ src/com/bridgelabz/Operation.java
@@ -1,7 +1,7 @@
 package com.bridgelabz;
 
 public class Operation {
-	 public Stack addElement() {
+	   public Stack addElement() {
 	        Stack stackList =new Stack();
 	        stackList.push(70);
 	        stackList.push(30);
@@ -9,6 +9,22 @@ public class Operation {
 
 	        stackList.display();
 	        return stackList;
+	    }
 
-	 }
+	    public void peek(){
+	        Stack stackList =new Stack();
+	        stackList.push(70);
+	        stackList.push(30);
+	        stackList.push(56);
+	        stackList.peek();
+	    }
+
+	    public void pop(){
+	        System.out.println("Before Pop:");
+	        Stack stackList =addElement();
+	        stackList.pop();
+	        System.out.println("");
+	        System.out.println("After Pop:");
+	        stackList.display();
+	    }
 }
--- src/com/bridgelabz/Stack.java
+++ src/com/bridgelabz/Stack.java
@@ -2,41 +2,57 @@ package com.bridgelabz;
 
 public class Stack {
 	Node head;
-    Node tail;
-    Node top;
+	Node tail;
+	Node top;
+	
+	public Node push(int data) {
+		Node newNode = new Node(data);
+		
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		}
+		else {
+			Node tempNode = head;
+			this.head = newNode;
+			newNode.next = tempNode;
+		}
+		top=head;
+		return newNode;
+	}
 
-    public Node push(int data) {
-        Node newnode = new Node(data);
+	public int peek(){
+		System.out.println("Top element is "+this.top.data);
+		return top.data;
+	}
 
-        if (head == null) {
-            head = newnode;
-            tail = newnode;
-        } else {
-            Node tempNode = head;
-            this.head = newnode;
-            newnode.next = tempNode;
-        }
-        top=head;
-        return newnode;
-    }
+	public void pop() {
+		Node temp =top;
+		while (temp != null){
+			Node tempNode = this.head;
+			this.head = tempNode.next;
+			temp=temp.next;
+		}
+	}
 
-    public void display(){
-        if (head == null) {
-            System.out.println("Linked list is Empty");
-            return;
-        } else {
-            System.out.print("\n The stack elements are ");
-            Node tempNode = head;
-            while (tempNode != null) {
-                if(tempNode.next !=null) {
-                    System.out.print(tempNode.data + " -> ");
-                    tempNode = tempNode.next;
-                }
-                else {
-                    System.out.print(tempNode.data);
-                    tempNode=tempNode.next;
-                }
-            }
-        }
-    }
+	public void display(){
+		if (head == null) {
+			System.out.println("Linked list is Empty");
+			return;
+		} 
+		else {
+			System.out.print("\n The stack elements are ");
+			Node tempNode = head;
+			while (tempNode != null) {
+				if(tempNode.next !=null) {
+					System.out.print(tempNode.data + " -> ");
+					tempNode = tempNode.next;
+				}
+				else {
+					System.out.print(tempNode.data);
+					tempNode=tempNode.next;
+				}
+			}
+		}
+	}
 }
